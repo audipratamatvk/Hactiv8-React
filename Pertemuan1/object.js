a0c463123ba3f34eca5c673a648ceba77ceffaa5
Muhammad Audi Agung Pratama <audi.pratama@traveloka.com>
2024-01-30 21:48:02 +0700

    lanjutan

---
+++ Pertemuan1/object.js
@@ -0,0 +1,11 @@
+let biodata = {
+    nama : "Luffy",
+    age : 17,
+    character: "One Piece",
+    status : true,
+    "full name": "Luffy D Monkey"
+}
+
+console.log(biodata["nama"])
+console.log(biodata.nama)
+console.log(biodata["full name"])
